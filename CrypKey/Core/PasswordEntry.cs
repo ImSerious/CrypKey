a749dad5fd4f70e23eb5ac8aeafd025802fd500d
Anthony <anthony.jaeger@imerir.com>
2018-01-31 14:49:39 +0100

    Propriétés PassWordEntry

--- CrypKey/Core/PasswordEntry.cs
+++ CrypKey/Core/PasswordEntry.cs
@@ -8,6 +8,17 @@ namespace CrypKey.Core
 {
     public class PasswordEntry
     {
+        // Propriétés
+        public string passWord { get; set; }
+        public string webSite { get; set; }
+        public string note { get; set; }
+
+        public PasswordEntry()
+        {
+            
+        }
+          
+        // Methode
 
     }
 }
