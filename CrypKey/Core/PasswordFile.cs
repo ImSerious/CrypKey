f940164543f8580037e6b0eaeaafd707946b712c
ImSerious <deaman57100@hotmail.com>
2018-02-03 17:02:35 +0100

    [UPDATE] Created project structure.

---
+++ CrypKey/Core/PasswordFile.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrypKeyWPF.Core
+{
+    /// <summary>
+    /// A file that contains every entries.
+    /// </summary>
+    public class PasswordFile
+    {
+        string password;
+        string note;
+        List<PasswordEntry> entries;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PasswordFile()
+        {
+            entries = new List<PasswordEntry>();
+        }
+
+        /// <summary>
+        /// Add an existing entry to the file.
+        /// </summary>
+        /// <param name="entry">Existing entry to add.</param>
+        public void Add(PasswordEntry entry)
+        {
+            entries.Add(entry);
+        }
+
+        /// <summary>
+        /// Add a new entry to the file.
+        /// </summary>
+        /// <param name="password">Password of the entry.</param>
+        /// <param name="website">Website of the entry.</param>
+        /// <param name="note">Note of the entry.</param>
+        public void Add(string password, string website, string note)
+        {
+            PasswordEntry entry = new PasswordEntry(password, website, note);
+
+            Add(entry);
+        }
+
+        /// <summary>
+        /// Remove an entry from the file.
+        /// </summary>
+        /// <param name="entry">Entry to remove.</param>
+        public void Remove(PasswordEntry entry)
+        {
+
+        }
+
+        /// <summary>
+        /// Remove an entry from the file.
+        /// </summary>
+        /// <param name="index">Index of the entry.</param>
+        public void Remove(int index)
+        {
+
+        }
+
+        /// <summary>
+        /// Encrypt an entry.
+        /// </summary>
+        /// <param name="entry">Entry to encrypt.</param>
+        /// <param name="index">Current index.</param>
+        public void Encrypt(PasswordEntry entry,int index)
+        {
+
+        }
+
+        /// <summary>
+        /// Decrypt an entry.
+        /// </summary>
+        /// <param name="cryptedEntry">Crypted entry.</param>
+        /// <param name="index">Current index.</param>
+        public void Decrypt(string cryptedEntry, int index)
+        {
+
+        }
+
+        /// <summary>
+        /// Load the file from a path.
+        /// </summary>
+        /// <param name="path">Path of the file.</param>
+        /// <returns>Loaded password file.</returns>
+        public static PasswordFile Load(string path)
+        {
+            PasswordFile file = null;
+
+            return file;
+        }
+
+        /// <summary>
+        /// Save the file.
+        /// </summary>
+        /// <param name="path">Path of the file.</param>
+        public void Save(string path)
+        {
+
+        }
+
+        /// <summary>
+        /// Get or set the password of the file.
+        /// </summary>
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        /// <summary>
+        /// Get or set the note of the file.
+        /// </summary>
+        public string Note
+        {
+            get { return note; }
+            set { note = value; }
+        }
+
+        /// <summary>
+        /// Get the entries of the password file.
+        /// </summary>
+        public List<PasswordEntry> Entries
+        {
+            get { return entries; }
+        }
+    }
+}
--- CrypKey/MainWindow.xaml.cs
+++ CrypKey/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CrypKeyWPF.Core;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +23,8 @@ namespace CrypKeyWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        string pathFile;
+
         /// <summary>
         /// Constructor.
         /// </summary>
@@ -33,7 +38,14 @@ namespace CrypKeyWPF
         /// </summary>
         private void OpenClick(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog dialog = new OpenFileDialog();
 
+            if (dialog.ShowDialog() == true)
+            {
+                textBoxPath.Text = pathFile = dialog.FileName;
+
+                buttonAdd.IsEnabled = buttonRemove.IsEnabled = buttonSave.IsEnabled = buttonSettings.IsEnabled = true; // Could be replaced by binding a check on PassworFile or filePath
+            }
         }
 
         /// <summary>
@@ -41,7 +53,30 @@ namespace CrypKeyWPF
         /// </summary>
         private void SaveClick(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog dialog = new SaveFileDialog();
 
+            if (dialog.ShowDialog() == true)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Click on the Settings button.
+        /// </summary>
+        private void SettingsClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// Click on a website in password entry.
+        /// </summary>
+        private void WebsiteClick(object sender, MouseButtonEventArgs e)
+        {
+            var test = (TextBlock)sender;
+
+            Process.Start(test.Text);
         }
     }
 }
